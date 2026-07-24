ee8a28bef00cdebb04084d011cb4cdf341d5e541
Bibek Giri <giribibek01@gmail.com>
2026-07-24 08:49:46 +0545

    Add OrderConformationScreen and update main app entry point to navigate to it

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -14,18 +14,94 @@ class CartScreen extends StatelessWidget {
         title: Text("Cart"),
       ),
 
-      body: ListView.builder(
-        itemCount: productData.length,
-        scrollDirection: Axis.vertical,
+      body: Column(
+        children: [
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: productData.length,
+            scrollDirection: Axis.vertical,
 
-        itemBuilder: (context, index) {
-          return Container(
-            margin: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-            height: 150,
-            width: double.maxFinite,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                height: 150,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadiusGeometry.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withValues(alpha: 0.3),
+                      blurRadius: 4,
+                      spreadRadius: 0.5,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset("assets/apple.png"),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Apple",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "1kg",
+                              style: TextStyle(color: Colors.blueGrey),
+                            ),
+                            Text(
+                              "₹150.00",
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 10,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.add_circle, color: Colors.green),
+                              Text("1"),
+                              Icon(Icons.remove_circle, color: Colors.green),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+
+          Container(
+            height: 70,
+            width: 300,
             decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadiusGeometry.circular(10),
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(30),
               boxShadow: [
                 BoxShadow(
                   color: Colors.black.withValues(alpha: 0.3),
@@ -35,51 +111,18 @@ class CartScreen extends StatelessWidget {
                 ),
               ],
             ),
-
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.asset("assets/apple.png"),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Apple",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text("1kg", style: TextStyle(color: Colors.blueGrey)),
-                        Text(
-                          "₹150.00",
-                          style: TextStyle(
-                            color: Colors.green,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+            child: Center(
+              child: Text(
+                "Checkout",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Row(
-                    children: [
-                      Icon(Icons.add_circle, color: Colors.green),
-                      Text("1"),
-                      Icon(Icons.remove_circle, color: Colors.green),
-                    ],
-                  ),
-                ),
-              ],
+              ),
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
---
+++ lib/screens/cart/order_conformation_screen.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+
+class OrderConformationScreen extends StatelessWidget {
+  const OrderConformationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+
+        title: Text(
+          "Order Conformation",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Your Details",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.pin_drop, size: 32),
+                    SizedBox(width: 20),
+                    Row(
+                      children: [
+                        Text(
+                          "Naya Gauu 15, Pokhara",
+                          style: TextStyle(color: Colors.blueGrey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Text("Edit", style: TextStyle(color: Colors.green)),
+              ],
+            ),
+
+            Divider(color: Colors.black, thickness: 1),
+
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.call, size: 32),
+                    SizedBox(width: 20),
+                    Row(
+                      children: [
+                        Text(
+                          "9869******",
+                          style: TextStyle(color: Colors.blueGrey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Text("Edit", style: TextStyle(color: Colors.green)),
+              ],
+            ),
+
+            Divider(color: Colors.black, thickness: 1),
+
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.mail, size: 32),
+                    SizedBox(width: 20),
+                    Row(
+                      children: [
+                        Text(
+                          "giribibek01@gmail.com",
+                          style: TextStyle(color: Colors.blueGrey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Text("Edit", style: TextStyle(color: Colors.green)),
+              ],
+            ),
+
+            SizedBox(height: 40),
+
+            Text(
+              "Abour Order",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.local_shipping, size: 32),
+                    SizedBox(width: 20),
+                    Row(
+                      children: [
+                        Text(
+                          "Delivery",
+                          style: TextStyle(color: Colors.blueGrey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [Text("Select Method"), Icon(Icons.chevron_right)],
+                ),
+              ],
+            ),
+
+            Divider(color: Colors.black, thickness: 1),
+
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.payment, size: 32),
+                    SizedBox(width: 20),
+                    Row(
+                      children: [
+                        Text(
+                          "Payment Method",
+                          style: TextStyle(color: Colors.blueGrey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Icon(Icons.chevron_right),
+              ],
+            ),
+
+            Divider(color: Colors.black, thickness: 1),
+
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.local_offer, size: 32),
+                    SizedBox(width: 20),
+                    Row(
+                      children: [
+                        Text(
+                          "Total Cost",
+                          style: TextStyle(color: Colors.blueGrey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text("₹160.00", style: TextStyle(color: Colors.green)),
+                    Icon(Icons.chevron_right),
+                  ],
+                ),
+              ],
+            ),
+
+            SizedBox(height: 100),
+
+            Center(
+              child: Container(
+                height: 70,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withValues(alpha: 0.3),
+                      blurRadius: 4,
+                      spreadRadius: 0.5,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Text(
+                    "Confirm Order",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
